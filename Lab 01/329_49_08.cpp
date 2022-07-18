cec74394da509e8e0cfacc121d2d324a9d2569c6
MD Rakibul Alam Emon <109478306+Rakibulemon329@users.noreply.github.com>
2022-07-18 23:56:41 +0600

    Add files via upload

---
+++ Lab 01/329_49_08.cpp
@@ -0,0 +1,13 @@
+
+//codeforce Porblem No = 4A
+//Problem Name =wartermelon
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int w=0; cin >> w;
+    string output = (w <=2 || w % 2) ? "NO": "YES";
+    cout << output << endl;
+ return 0;
+ }
