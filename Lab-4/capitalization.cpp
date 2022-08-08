f7318d71fc0844c685696313756096adf3eb8452
MD Rakibul Alam Emon <109478306+Rakibulemon329@users.noreply.github.com>
2022-08-08 22:41:14 +0600

    Add files via upload

---
+++ Lab-4/capitalization.cpp
@@ -0,0 +1,17 @@
+//Codeforces problem no-281A
+//Problem name-Word capitalization
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    s[0]=towupper(s[0]);
+    cout<<s;
+    return 0;
+}
+
