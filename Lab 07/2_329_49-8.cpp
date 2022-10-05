2d5aedc73e1605cf95a9b5d73c69cac57d349382
MD Rakibul Alam Emon <109478306+Rakibulemon329@users.noreply.github.com>
2022-10-06 00:14:01 +0600

    Add files via upload

---
+++ Lab 07/2_329_49-8.cpp
@@ -0,0 +1,23 @@
+//codeforces Problem No = 116A
+//Problem Name =Tram
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a,b;
+    int sum=0,k=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a>>b;
+        sum=(a+k)-b;
+        if(sum<0)
+            k=0;
+        else
+        k=sum;
+    }
+    cout<<sum;
+
+}
+
